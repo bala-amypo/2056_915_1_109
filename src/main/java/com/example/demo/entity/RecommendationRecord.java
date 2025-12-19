91b620e538788bdc6a40917f2a4e715915860808
bala-amypo <bala@amypo.in>
2025-12-19 10:22:59 +0000

    User commit

--- src/main/java/com/example/demo/entity/RecommendationRecord.java
+++ src/main/java/com/example/demo/entity/RecommendationRecord.java
@@ -31,7 +31,7 @@ public class RecommendationRecord {
 
     public RecommendationRecord() {}
 
-    // Getters and Setters
+    
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
--- src/main/java/com/example/demo/entity/RewardRule.java
+++ src/main/java/com/example/demo/entity/RewardRule.java
@@ -23,7 +23,7 @@ public class RewardRule {
 
     public RewardRule() {}
 
-    // Getters and Setters
+    
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
