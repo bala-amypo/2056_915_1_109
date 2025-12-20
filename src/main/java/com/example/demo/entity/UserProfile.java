d8ea7aa77901e72b9260c1828c398500f01fe643
bala-amypo <bala@amypo.in>
2025-12-20 05:46:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserProfile.java
+++ src/main/java/com/example/demo/entity/UserProfile.java
@@ -42,8 +42,7 @@ public class UserProfile {
     }
 
     public UserProfile() {}
-
-    /* Getters and Setters */
+    
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
