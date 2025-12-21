9f3604c12ce6c7f8629ebce31e18549240f702a9
bala-amypo <bala@amypo.in>
2025-12-21 13:53:27 +0000

    User commit

--- src/main/java/com/example/demo/service/RecommendationEngineService.java
+++ src/main/java/com/example/demo/service/RecommendationEngineService.java
@@ -3,7 +3,8 @@ package com.example.demo.service;
 import com.example.demo.entity.RecommendationRecord;
 import java.util.List;
 
-public interface RecommendationEngineService {
+@Service
+public class RecommendationEngineServiceImpl implements RecommendationEngineService{
 
     RecommendationRecord generateRecommendation(Long intentId);
 
