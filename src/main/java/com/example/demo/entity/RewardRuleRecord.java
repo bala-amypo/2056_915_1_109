433abbe70a8a77e7007482c08c51142156974eba
bala-amypo <bala@amypo.in>
2025-12-21 14:11:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/RewardRuleRecord.java
+++ src/main/java/com/example/demo/entity/RewardRuleRecord.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
     name = "reward_rules",
     uniqueConstraints = @UniqueConstraint(columnNames = {"cardId", "category"})
 )
-public class RewardRule {
+public class RewardRuleRecord {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
