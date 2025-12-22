e7fb5e6cbb601dd300fb33468e5382d99853d947
bala-amypo <bala@amypo.in>
2025-12-22 09:35:58 +0000

    User commit

--- src/main/java/com/example/demo/service/CreditCardService.java
+++ src/main/java/com/example/demo/service/CreditCardService.java
@@ -3,7 +3,9 @@ package com.example.demo.service;
 import com.example.demo.entity.CreditCardRecord;
 
 import java.util.List;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface CreditCardService {
 
     CreditCardRecord addCard(CreditCardRecord card);
