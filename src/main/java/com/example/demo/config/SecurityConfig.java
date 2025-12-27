8b9bcd0df768d491e42cb57b08b3049fcb87a228
bala-amypo <bala@amypo.in>
2025-12-27 05:30:18 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,17 +1,24 @@
 package com.example.demo.config;
 
-import com.example.demo.security.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.nio.charset.StandardCharsets;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
-    
+
     @Bean
-    public JwtUtil jwtUtil() {
-        byte[] secret = "credit-card-reward-maximizer-jwt-secret-key-2024".getBytes(StandardCharsets.UTF_8);
-        return new JwtUtil(secret, 24L * 60L * 60L * 1000L); // 24 hours
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
